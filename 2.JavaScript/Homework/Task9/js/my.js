e9440c4d2ee403a03c312f91f00bbd849c2eb272
Lilit <atanesyanlilit09@gamil.com>
2020-08-16 14:45:30 +0300

    add my.js

---
+++ 2.JavaScript/Homework/Task9/js/my.js
@@ -0,0 +1,15 @@
+		var number = 11;
+
+		for(var i = 0; i < number; i++){
+			document.write("Numder больше нуля<br>");
+			number = number - 1;
+		}
+		document.write("Numder меньше нуля<br>"); 
+
+		var apples = 5;
+
+		for(var i = 0; apples < 10; i++){
+			document.write("Дай мне еще яблок <br>");
+			apples = apples + 1;
+		}
+		document.write("Больше яблок мне не надо");
