515cab0642dd624f8f6ff79671f562f93d30ef88
Lilit <atanesyanlilit09@gamil.com>
2020-08-16 17:56:53 +0300

    add my.js

---
+++ 2.JavaScript/Homework/Task10/js/my.js
@@ -0,0 +1,17 @@
+var kitty = {
+	animal: "Cat",
+	pow: 4,
+	year: 3,
+	coat_color: "ginger",
+	eye_color: "green",
+	fav_food: "little mice"
+	};
+
+var doggy = {
+	make: "Dog",
+	pow: 4,
+	year: 7,
+	coat_color: "black&white",
+	eye_color: "blue",
+	fav_food: "steak"
+};
